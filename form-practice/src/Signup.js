72745d8b7926a4b68fcdae7e73c735f7f1f33c24
aoavzm <aoavzm@gmail.com>
2022-09-06 21:59:01 -0700

    Long Pratice Done

--- form-practice/src/Signup.js
+++ form-practice/src/Signup.js
@@ -10,10 +10,49 @@ function Signup(){
     const [notifications, setNotifications] = useState(false)
     const [validationErrors, setValidationErrors] = useState([])
 
+    useEffect(() => {
+        const errors = new Array();
+        if(name.length <= 0) {
+          errors.push('Please enter your Name');
+        }
+        if(!email.includes('@')) {
+          errors.push('Please provide a valid Email');
+        }
+        if(phone.length > 0) {
+            if(phone.length !== 10) {
+                errors.push('Phone number should be properly formatted');
+            }
+            if(!phoneType.length) {
+                errors.push('Phone type should be selected');
+            }
+        }
+        if(bio.length > 280) {
+            errors.push('Bio is too long');
+        }
+        setValidationErrors(errors)
+    }, [name, email, phone, phoneType, bio])
+
+    function onSubmit(e) {
+        e.preventDefault();
+        const summitedOn = new Date()
+        console.log({ name, email, phone, phoneType, staff, bio, notifications, summitedOn });
+    }
 
     return (
         <div>
-            <form>
+            {
+                validationErrors.length > 0 && (
+                <div style={{color: 'red'}}>
+                    The following errors were found:
+                    <ul>
+                    {validationErrors.map(error => (
+                        <li key={error}>{error}</li>
+                    ))}
+                    </ul>
+                </div>
+                )
+            }
+            <form onSubmit={onSubmit}>
                 <div>
                     <label>
                         Name
@@ -46,13 +85,16 @@ function Signup(){
                         id='phone'
                         type='text'
                         name='phone'
+                        placeholder='1234567890'
                         onChange={e => setPhone(e.target.value)}
                         value={phone}
                     />
                     <select
+                        id='phoneType'
                         name='phoneType'
                         onChange={e => setPhoneType(e.target.value)}
                         value={phoneType}
+                        disabled={phone.length === 0}
                     >
                         <option disabled value=''>Choose a phone type</option>
                         <option>Home</option>
@@ -64,21 +106,27 @@ function Signup(){
                     <label>
                         Staff
                     </label>
-                    <input type="radio" value="Instructor" name="staff" /> Instructor
-                    <input type="radio" value="Student" name="staff" /> Student
+                    <input type="radio" value="Instructor" name="staff" onChange={e => setStaff(e.target.value)} /> Instructor
+                    <input type="radio" value="Student" name="staff" onChange={e => setStaff(e.target.value)} /> Student
                 </div>
                 <div>
                     <label>
                         Bio
                     </label>
-                    <textarea />
+                    <textarea
+                        id='bio'
+                        name='bio'
+                        onChange={e => setBio(e.target.value)}
+                        value={bio}
+                    />
                 </div>
                 <div>
                     <label>
                         Sign up for email notifications
                     </label>
-                    <input type='checkbox' />
+                    <input type='checkbox' name='notifications' onChange={e => setNotifications(!notifications)}/>
                 </div>
+                <button disabled={validationErrors.length > 0}>Sign Up</button>
             </form>
         </div>
     )
